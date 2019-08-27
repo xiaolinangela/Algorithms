67fee175d5a1a914d11666a11d9a0accf4f8104d
Angela Zhu <xiaolinangela@gmail.com>
2019-08-26 21:46:34 -0700

    HW3 Quicksort using first, last and median pivot

---
+++ hw3/hw3_1.py
@@ -0,0 +1,49 @@
+from sys import argv
+import math
+import sys
+# Use the first element as the pivot 
+comparisons = 0 
+def partition(numbers, left, right):
+    global comparisons
+    #print(numbers[left:right])
+    pivot = numbers[left]
+    i = left + 1
+    subarray_size = len(numbers[left:right])
+    comparisons = comparisons + subarray_size - 1
+    for j in range(i,right):
+        if numbers[j] < pivot:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            i += 1
+    numbers[left], numbers[i-1] = numbers[i-1], numbers[left]
+    return i-1
+
+def quicksort(numbers,left,right):
+    if left < right:
+        split = partition(numbers,left,right)
+        quicksort(numbers,left,split)
+        quicksort(numbers,split+1,right)
+    return numbers
+
+def main():
+    if len(sys.argv) == 2:
+        texts = sys.argv[1]
+        numbers = []
+        # read the numbers from the file and store them in a list
+        with open(texts, 'r') as file:
+            for line in file:
+                numbers.append(line.rstrip("\n"))
+        # convert strings to numbers
+        numbers = [int(i) for i in numbers]
+        i = numbers[0]
+        # call the quicksort function
+        left = 0
+        right = len(numbers)
+        quicksort(numbers,left,right)
+        #print(numbers)
+        print(comparisons)
+    else:
+        print("Error: Add a file")
+
+
+if __name__ == "__main__":
+    main()

bcb41dbf2a1a820d7d93f4caef1bf1b2e4c9acd8
Angela Zhu <xiaolinangela@gmail.com>
2019-11-16 15:41:27 -0800

    strongly connected components algorithm

--- Part2_HW1/SCC_hw1_part2.py
+++ Part2_HW1/SCC_hw1_part2.py
@@ -1,5 +1,5 @@
 import sys, threading 
-# sys.setrecursion(800000)
+sys.setrecursionlimit(800000)
 threading.stack_size(67108864)
 
 def build_graph(filename):
@@ -27,67 +27,85 @@ def build_graph_rev(filename):
 
 def SCC(graph, graph_rev):
 
-	explored_nodes_rev = None
-	explored_nodes=None
+	explored_nodes_rev = set()
+	explored_nodes= set()
 	stack = []
 	# for finishing times in 1st pass
 	t = 0 
 	#for leaders in 2nd pass
 	leaders = {}
+	current_leader = 0
 
 	def toposort(graph_rev, v):
-		nonlocal explored_nodes_rev
+		nonlocal explored_nodes_rev, stack
 		explored_nodes_rev.add(v)
-		for i in graph_rev[v]:
-			if i not in explored_nodes_rev:
-				toposort(graph_rev, i)
+		#print(v)
+		if v in graph_rev:
+			for i in graph_rev[v]:
+				if i not in explored_nodes_rev:
+					toposort(graph_rev, i)
 		stack.insert(0,v)
 
 	def toposort_loop(graph_rev): 
 		nonlocal explored_nodes_rev, stack
-		print(graph_rev.key)
-		total_nodes = len(graph_rev)
-		for i in range(1,total_nodes+1):
+		#total_nodes = len(graph_rev)
+		#print(total_nodes)
+		nodes = list(graph_rev.keys())
+		#print(nodes)
+		for i in nodes:
 			if i not in explored_nodes_rev:
-				explored_nodes.add(i)
+				explored_nodes_rev.add(i)
 				toposort(graph_rev, i)
 
 	def DFS(graph, v):
-		nonlocal explored_nodes, t, leaders
+		nonlocal explored_nodes, t, leaders, current_leader
 		explored_nodes.add(v)
-		for i in graph[v]:
-			if i not in explored_nodes:
-				dest_nodes = leaders.get(v, set())
-				dest_nodes.add(i)
-				leaders[v] = dest_nodes
-				DFS(graph,i)
+		if v in graph: 
+			for i in graph[v]:
+				if i not in explored_nodes:
+					dest_nodes = leaders.get(current_leader, set())
+					dest_nodes.add(i)
+					leaders[current_leader] = dest_nodes
+					DFS(graph,i)
 
 	def DFS_loop(graph):
-		nonlocal explored_nodes, t, leaders
+		nonlocal explored_nodes, t, leaders, current_leader
 		for i in stack: 
 			if i not in explored_nodes:
+				explored_nodes.add(i)
+				leaders[i] = set()
+				current_leader = i
 				DFS(graph,i)
 
 	toposort_loop(graph_rev)
 	DFS_loop(graph)
-	return list(leaders.values())
+	#print(stack)
+	SCC_size = []
+	for key in leaders:
+		SCC_size.append(len(leaders[key])+1)
+	result = sorted(SCC_size)
+	result.reverse()
+	print(result[0:5])
+	#print(leaders)
+	return leaders
 
 def main():
 	if len(sys.argv) == 2:
-		txt_file = sys.argv[1];
+		txt_file = sys.argv[1]
+		#print("hello world")
 		graph = build_graph(txt_file)
+		#print("hello world 2")
 		graph_rev = build_graph_rev(txt_file)
-		print(graph_rev)
-		print(len(graph_rev))
+		#print(graph)
+		#print(graph_rev)
 		SCC(graph, graph_rev)
-		print(leaders.values())
+		#print(leaders)
 	else:
 		print("Error: Add a file")
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+thread = threading.Thread(target=main)
+thread.start()
 
 
-#thread = threading.Thread(target=main)
-#thread.start()
